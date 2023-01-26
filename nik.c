a9e53de4fc1ab72b726ac5eb7f1ae123cd9c244c
Yashmita Sharma <71992108+xyashmitax@users.noreply.github.com>
2023-01-26 16:59:16 -0500

    Create nik.c

---
+++ nik.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int
+main (void)
+{
+	int a, b, aa, bb, y;
+printf("Enter value of side length 'a': ");
+	scanf("%lf", &a);
+	/* obtained value for a */
+	
+printf("Enter value of side length 'b': ");
+	scanf("%lf", &b);
+	/* obtained value for b */
+
+aa = a * a;
+	/* value for a*a */
+	scanf("%d", &aa);
+
+bb = b * b;
+	/* value for b*b */
+	scanf("%d", &bb);
+	
+y = aa + bb;
+	/* value for c^2 */
+	scanf("%d", &y);
+	
+printf("the value for y: \n");
+
+}
